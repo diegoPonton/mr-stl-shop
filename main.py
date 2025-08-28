1aa4c0308a7174f97106d58f1e27adcc1c731c16
Diego P. <89942112+diegoPonton@users.noreply.github.com>
2025-08-27 20:53:24 -0500

    Update main.py
    configuracion puerto railway

--- main.py
+++ main.py
@@ -253,5 +253,8 @@ def eliminar_producto(id):
 
 # ---------------------------------------------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
